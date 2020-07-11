870f9626e0a74433018449103ad7e616a109302a
unknown <chaochinshin@gmail.com>
2020-07-11 13:02:33 -0400

    app.js nav.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-// import 404 from './pages/404';
-// import Nav from './components/Nav'
+import NoMatch from './pages/404';
+import Nav from './components/Nav'
 import './App.css';
-
 function App() {
   return (
     <Router>
       <div>
+        <Nav />
         <Switch>
-          <Route exact path={["/", "/signup"]}>
+          <Route exact path={["/", "home"]}>
+            <Home />
+          </Route>
+          <Route exact path="/signup">
             <SignUp />
           </Route>
           <Route exact path="/login">
             <Login />
+          </Route>
+          <Route>
+            <NoMatch />
           </Route>
         </Switch>
       </div>
     </Router>
   );
 }
-
 export default App;
