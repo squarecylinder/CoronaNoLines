e82af872efcdcd72ec4e090b100774c13fb83ff1
JHauck56 <jhauck56@gmail.com>
2020-07-16 18:02:23 -0400

    adding current location marker to map

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import { GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const mapStyles = {
-  width: '600px',
-  height: '600px'
-};
+import CurrentLocation from '../components/Map/Map';
+
 
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      stores: [{lat: 40.4173, lng: -82.9071},
-              {latitude: 40.4173, longitude: -82.9071},
-              {latitude: 41.505493, longitude: -81.681290},
-              {latitude: 39.983334, longitude: -82.983330}
-              ],
               showingInfoWindow: false,  //Hides or the shows the infoWindow
               activeMarker: {},          //Shows the active marker upon click
-              selectedPlace: {}
+              selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
     }
   }
   onMarkerClick = (props, marker, e) =>
@@ -37,31 +30,16 @@ onClose = props => {
   }
 };
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => this.onMarkerClick} />
-    })
-  }
   
 
   render() {
     return (
-        <Map
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: 40.4173, lng: -82.9071}}
-        >
-          {this.displayMarkers()}
-          <Marker
-          onClick={this.onMarkerClick}
-          name={'Info Window'}
-          />
-          <InfoWindow
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
           onClose={this.onClose}
@@ -69,9 +47,8 @@ onClose = props => {
           <div>
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
-          </InfoWindow>
- 
-        </Map>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
