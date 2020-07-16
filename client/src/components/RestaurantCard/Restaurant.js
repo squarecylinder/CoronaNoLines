54589b0eb16dd11de5841fc915cc232a322245f2
HD1G <rhashier@oh.rr.com>
2020-07-16 13:27:06 -0400

    Front End Home Page / New Components / CSS

---
+++ client/src/components/RestaurantCard/Restaurant.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './restaurant.css'
+
+//Need to dynamically inject fields from MongoDb
+function Restaurant() {
+    return (
+    <div className="col-4" id="restaurant-card">
+<div className="card">
+  <h5 className="card-header restaurant-header">Insert Restaurant Name Here.</h5>
+  <div className="card-body">
+      <div>
+          insert map here.
+      </div>
+    <h6>Insert Address Here.</h6>
+    <h6>Insert Dine-In.</h6>
+    <h6>Insert Table Count.</h6>
+    <h6>Insert Outside Dining.</h6>
+    <h6>Insert Drive-Thru.</h6>
+    <h6>Insert Take-Out.</h6>
+    <h6>Insert Masks.</h6>
+  </div>
+  <div className="card-footer restaurant-footer">
+    Last updated: Insert "User Updated"
+  </div>
+</div>
+</div>
+
+  );
+}
+
+export default Restaurant;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Retail from '../components/RetailCard/Retail'
+import Restaurant from '../components/RestaurantCard/Restaurant'
+import Jumbotron from '../components/Jumbotron/Jumbotron'
 
+//Need to iterate to dynamically generate new cards
 function Home() {
     return (
-        <div>This is our homepage.</div>
+        <div className="Container">
+            <div className="row">
+            <Jumbotron />
+                </div>
+ 
+            <div className="row">
+            <Retail />
+            <Retail />
+            <Retail />
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
+             </div>
+        </div>
     )
 }
 export default Home;
