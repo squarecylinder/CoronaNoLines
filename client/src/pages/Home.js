b0cbb68197acd4406665e63cb69220aba31217ad
unknown <chaochinshin@gmail.com>
2020-07-16 18:57:38 -0400

    updating err

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,15 +7,14 @@ import Jumbotron from '../components/Jumbotron/Jumbotron'
 
 //Need to iterate to dynamically generate new cards
 function Home() {
-    let businesses = [1,2,3];
-    const businessCards = businesses.map(business => <li className = "col-md-3"> <BusinessCard /></li>);
+
     return (
 
         <div className="Container">
             <div className="row">
             <Jumbotron />
                 </div>
-                <ul className = "row">{businessCards}</ul>
+ 
             <div className="row">
             <Retail />
             <Retail />
