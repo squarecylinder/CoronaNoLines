eb03bfb2823562f7a5e97776188fadbb01bb9e53
unknown <chaochinshin@gmail.com>
2020-07-20 21:16:04 -0400

    accountpage

--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Restaurant from '../components/RestaurantCard/Restaurant';
+import Retail from '../components/RetailCard/Retail';
 
 function AccountPage() {
+    const [editing, setediting] = useState (false)
+    const [isrestaurant,setrestaurant] = useState (true)
+    function handleeditclick () {
+        setediting(true)
+    }
+    function save () {
+        setediting(false)
+    }
+    let renderCard;
+    let business;
+if(isrestaurant) {
+    business = <Restaurant />
+}
+else{
+    business = <Retail />
+
+}
+if(editing) {
+    renderCard = <form><button onClick = {save}>save detail</button></form>
+}
+else{ 
+    renderCard =     <div>
+        {
+            business
+        }
+   
+    <button onClick = {handleeditclick}>Edit Restaurant Information</button>
+</div>
+}
     return (
-        <div>This is the existing account page.</div>
+        <div>
+{
+    renderCard
+}
+        </div>
     )
 }
-export default AccountPage;
+export default AccountPage; 
