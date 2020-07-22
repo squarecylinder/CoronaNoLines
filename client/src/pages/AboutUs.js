bbc64d9dfbb74b3b66bc44621345a2d817a23b24
unknown <chaochinshin@gmail.com>
2020-07-21 21:54:09 -0400

    contact us updates

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -5,12 +5,25 @@ function AboutUs() {
     return (
         <div className="jumbotron jumbotron-fluid bg">
             <div className="container">
-                <h1 className="display-4">Latest Covid-19 updates</h1>
-                <p className="lead">Provide latest business operating hours and Covid etiquettes needed.</p>
-                <a href="/"><button className="btn btn-primary">Home</button></a>
+                 <h1 className="display-4 text-danger"><strong>Latest Covid-19 updates</strong></h1>
+                <p className="display-4 text-danger"><strong>Provide latest business operating hours and Covid etiquettes needed.</strong></p>
+                <a href="/"><button className="btn btn-danger">Home</button></a>
+                 
+           
+              <h2 className="display-5 text-danger"><strong>Project Three Team Members: Jalen Straka, Ryan Hashier, Jesse Hauck, ChinShin Chao</strong></h2>
+              <p className="display-5 text-danger"><strong>Covid-19 World</strong></p>
+              <p className="display-5 text-danger"><strong>Our Purpose</strong></p>
+              <p className="display-5 text-danger"><strong>We create this website to connect individuals to businesses locally providing latest business information for customers.</strong></p>
+                  
+          
+              <h2 className="display-5 text-danger"><strong>Please contact us when you experience difficulties with the website.</strong></h2>
+              <p></p>
+              <p className="display-5 text-danger"><strong>24 hours support service line: (216) 444-5555</strong></p>
+              <p className="display-5 text-danger"><strong>fullstack.team4@fullstackemail.com</strong></p>
+              <p><button className="btn btn-danger">Contact</button></p>
             </div>
-        </div>
-    )
-}
+          </div>
+        
+    )} 
 
 export default AboutUs;
