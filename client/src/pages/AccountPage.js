91ef5630fb7a0ebf5da37136cafa91fcd8aa3676
Jalen Straka <ziencck@gmail.com>
2020-07-21 20:58:13 -0400

    login takes you to your account page

--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -1,45 +1,14 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Restaurant from '../components/RestaurantCard/Restaurant';
 import Retail from '../components/RetailCard/Retail';
 import Profile from '../components/RestaurantProfile/RestaurantProfile'
 
 function AccountPage() {
-    const [editing, setediting] = useState (false)
-    const [isrestaurant,setrestaurant] = useState (true)
-    function handleeditclick () {
-        setediting(true)
-    }
-    function save () {
-        setediting(false)
-    }
-    let renderCard;
-    let business;
-if(isrestaurant) {
-    business = <Restaurant />
-}
-else{
-    business = <Retail />
 
-}
-if(editing) {
-    renderCard = <form><button onClick = {save}>save detail</button></form>
-}
-else{ 
-    renderCard =     <div>
-        {
-            business
-        }
-   
-    <button onClick = {handleeditclick}>Edit Restaurant Information</button>
-</div>
-}
     return (
       <div className="container">
           <div>
             <Profile />
-          </div>
-          <div>
-            {renderCard}
           </div>
       </div>
     )
--- routes/api/login.js
+++ routes/api/login.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 router.post("/login", passport.authenticate("local"), (req, res) => {
     console.log("login successful")
     res.json(req.user)
+    console.log(req.user)
 })
 
 module.exports = router;
