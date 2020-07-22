80d194a3c3b84bd15a2f4b6063eb1da492d8dc08
Jalen Straka <ziencck@gmail.com>
2020-07-21 21:58:36 -0400

    adding new npm i stuff

--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState } from 'react';
-import Restaurant from '../components/RestaurantCard/Restaurant';
-import Retail from '../components/RetailCard/Retail';
+// import Restaurant from '../components/RestaurantCard/Restaurant';
+// import Retail from '../components/RetailCard/Retail';
 import Profile from '../components/RestaurantProfile/RestaurantProfile'
 
 function AccountPage() {
