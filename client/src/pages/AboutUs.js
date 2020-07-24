eed7615511c4b6b3fd5c5bdd29c6b3da4e1f474d
unknown <chaochinshin@gmail.com>
2020-07-24 19:02:18 -0400

    updated aboutus

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -6,34 +6,30 @@ function AboutUs() {
     return (
         <div>
             <div className="container">
-            <div className="row">
-                <AboutUsImg />
-            </div>
-            <div className="row">
-                <Aboutusjumbotron />
-            </div>
-            <div  className="row">
+           
 
-            </div>
-              <h1 className="display-6 text-primary"><strong>Latest Covid-19 updates</strong></h1>
-              <p className="display-6 text-primary"><strong>Provide latest business operating hours and Covid etiquettes needed.</strong></p>
-              <a><button className="btn btn-primary">Home</button></a>
+                <Aboutusjumbotron />
+
+            
+              <h1 className="display-6 blue"><strong>Latest Covid-19 updates</strong></h1>
+              <p className="display-6 blue"><strong>Provide latest business operating hours and Covid etiquettes needed.</strong></p>
+              <a><button className="btn blue">Home</button></a>
                  
            
-              <h2 className="display-6 text-primary"><strong>Project Three Team Members: Jalen Straka, Ryan Hashier, Jesse Hauck, ChinShin Chao</strong></h2>
-              <p className="display-6 text-primary"><strong>Covid-19 World</strong></p>
-              <p className="display-6 text-primary"><strong>Our Purpose</strong></p>
-              <p className="display-6 text-primary"><strong>We create this website to connect individuals to businesses locally providing latest business information for customers.</strong></p>
+              <p className="display-6 blue"><strong>Project Three Team Members: Jalen Straka, Ryan Hashier, Jesse Hauck, ChinShin Chao</strong></p>
+              <p className="display-6 blue"><strong>Covid-19 World</strong></p>
+              <p className="display-6 blue"><strong>Our Purpose</strong></p>
+              <p className="display-6 blue"><strong>We create this website to connect individuals to businesses locally providing latest business information for customers.</strong></p>
                   
           
-              <h2 className="display-6 text-primary"><strong>Please contact us when you experience difficulties with the website.</strong></h2>
-              <p></p>
-              <p className="display-6 text-primary"><strong>24 hours support service line: (216) 444-5555</strong></p>
-              <p className="display-6 text-primary"><strong>fullstack.team4@fullstackemail.com</strong></p>
-              <p><button className="btn btn-primary">Contact</button></p>
+              <p className="display-6 blue"><strong>Please contact us when you experience difficulties with the website.</strong></p>
+              
+              <p className="display-6 blue"><strong>24 hours support service line: (216) 444-5555</strong></p>
+              <p className="display-6 blue"><strong>fullstack.team4@fullstackemail.com</strong></p>
+              <p><button className="btn blue">Contact</button></p>
             </div>
           </div>
-        
+
     )} 
 
 export default AboutUs;
