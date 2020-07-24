9ffc2a62c6e72e4e27194746ea647b704fb124b1
unknown <chaochinshin@gmail.com>
2020-07-23 20:14:32 -0400

    aboutus pic

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,29 +1,40 @@
 import React from 'react';
-
+import Aboutusjumbotron from '../components/Jumbotron/Aboutusjumbotron'
+import AboutUsImg from '../components/AboutUsImg';
 
 function AboutUs() {
     return (
-        <div className="jumbotron jumbotron-fluid bg">
+        <div>
             <div className="container">
-                 <h1 className="display-4 text-danger"><strong>Latest Covid-19 updates</strong></h1>
-                <p className="display-4 text-danger"><strong>Provide latest business operating hours and Covid etiquettes needed.</strong></p>
-                <a href="/"><button className="btn btn-danger">Home</button></a>
+            <div className="row">
+                <AboutUsImg />
+            </div>
+            <div className="row">
+                <Aboutusjumbotron />
+            </div>
+            <div  className="row">
+
+            </div>
+              <h1 className="display-6 text-primary"><strong>Latest Covid-19 updates</strong></h1>
+              <p className="display-6 text-primary"><strong>Provide latest business operating hours and Covid etiquettes needed.</strong></p>
+              <a><button className="btn btn-primary">Home</button></a>
                  
            
-              <h2 className="display-5 text-danger"><strong>Project Three Team Members: Jalen Straka, Ryan Hashier, Jesse Hauck, ChinShin Chao</strong></h2>
-              <p className="display-5 text-danger"><strong>Covid-19 World</strong></p>
-              <p className="display-5 text-danger"><strong>Our Purpose</strong></p>
-              <p className="display-5 text-danger"><strong>We create this website to connect individuals to businesses locally providing latest business information for customers.</strong></p>
+              <h2 className="display-6 text-primary"><strong>Project Three Team Members: Jalen Straka, Ryan Hashier, Jesse Hauck, ChinShin Chao</strong></h2>
+              <p className="display-6 text-primary"><strong>Covid-19 World</strong></p>
+              <p className="display-6 text-primary"><strong>Our Purpose</strong></p>
+              <p className="display-6 text-primary"><strong>We create this website to connect individuals to businesses locally providing latest business information for customers.</strong></p>
                   
           
-              <h2 className="display-5 text-danger"><strong>Please contact us when you experience difficulties with the website.</strong></h2>
+              <h2 className="display-6 text-primary"><strong>Please contact us when you experience difficulties with the website.</strong></h2>
               <p></p>
-              <p className="display-5 text-danger"><strong>24 hours support service line: (216) 444-5555</strong></p>
-              <p className="display-5 text-danger"><strong>fullstack.team4@fullstackemail.com</strong></p>
-              <p><button className="btn btn-danger">Contact</button></p>
+              <p className="display-6 text-primary"><strong>24 hours support service line: (216) 444-5555</strong></p>
+              <p className="display-6 text-primary"><strong>fullstack.team4@fullstackemail.com</strong></p>
+              <p><button className="btn btn-primary">Contact</button></p>
             </div>
           </div>
         
     )} 
 
 export default AboutUs;
+//className="jumbotron jumbotron-fluid bg"
