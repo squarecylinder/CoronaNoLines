7b5361d3b7f2b0417e8486a1833ee3dee266e2b9
Jalen Straka <ziencck@gmail.com>
2020-07-25 11:49:11 -0400

    adding props

--- client/src/components/Jumbotron/Aboutusjumbotron.js
+++ client/src/components/Jumbotron/Aboutusjumbotron.js
@@ -5,7 +5,7 @@ import AboutUsImg from '../AboutUsImg.js'
 
 function Aboutusjumbotron() {
   return (
-    <div className="col" id="aboutusjumbotron-section" className="display-6 blue">
+    <div className="col display-6 blue" id="aboutusjumbotron-section">
       <AboutUsImg />
     <h2 className="aboutusjumbo-h2 display-6 blue text-center"><strong>All About Us</strong></h2>
     <p className="aboutusjumbo-p display-6 blue"><strong>We live in a Covid-19 World, and getting the most up-to-date information is critical when making decisions.  Even when simply choosing where to shop or eat. Whether you are a business looking to keep your customers in the loop, or a customer wondering if your favorite store or restaurant is implimenting safe, social distance policies, our database gives you all the most important information.</strong></p>
