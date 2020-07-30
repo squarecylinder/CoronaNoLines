f5d094c6c2df4de9c6adaa72f1285710d625af26
HD1G <rhashier@oh.rr.com>
2020-07-30 08:56:32 -0400

    cleaned up about us page

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ import './jumbotron.css';
 function Jumbotron() {
   return (
     <div className="col" id="jumbotron-section">
-    <h2 className="jumbo-h2">Welcome!</h2>
+    <h2 className="jumbo-h2">Welcome to Corona No Lines!</h2>
     <p className="jumbo-p">We live in a Covid-19 World, and getting the most up-to-date information is critical when making decisions.  Even when simply choosing where to shop or eat. Whether you are a business looking to keep your customers in the loop, or a customer wondering if your favorite store or restaurant is implimenting safe, social distance policies, our database gives you all the most important information.</p>
   </div>
 
